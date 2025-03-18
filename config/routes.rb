8bfc568208658a0faf8ed8178103138cb92acc0b
Jose Oliveira <jedpfo89@gmail.com>
2025-03-18 13:40:27 +0000

    basic show page template

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: "pages#home"
-  resources :clinics, only: :index
-  resources :doctors, only: :index
+  resources :clinics
+  resources :doctors
 end
