09dd3a2a05cb0dc3495529453d734dfba3bcd564
Jose Oliveira <jedpfo89@gmail.com>
2025-03-24 11:52:38 +0000

    more fixing profile and sign up

--- app/models/profile.rb
+++ app/models/profile.rb
@@ -2,4 +2,9 @@ class Profile < ApplicationRecord
   belongs_to :user
   has_one_attached :photo
   has_one_attached :document
+
+  validates :first_name, presence: true
+  validates :last_name, presence: true
+  validates :address, presence: true
+  validates :dob, presence: true
 end
