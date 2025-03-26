776463aaf0873eb65d9af1f5b8c035cfd8651066
Bilgin Bilir <bilgin.bilir@whu.edu>
2025-03-26 14:36:26 +0100

    Doctor descriptions. Added a new column to database

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.2].define(version: 2025_03_26_094121) do
+ActiveRecord::Schema[7.2].define(version: 2025_03_26_114959) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
   enable_extension "vector"
@@ -102,6 +102,7 @@ ActiveRecord::Schema[7.2].define(version: 2025_03_26_094121) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.vector "embedding", limit: 1536
+    t.text "description"
     t.index ["clinic_id"], name: "index_doctors_on_clinic_id"
     t.index ["user_id"], name: "index_doctors_on_user_id"
   end
